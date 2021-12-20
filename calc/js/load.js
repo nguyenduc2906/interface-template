186dc200f443f584e6894528cbfe00ae475a528c
nguyenduc2906 <93317735+nguyenduc2906@users.noreply.github.com>
2021-12-20 13:18:07 +0700

    interface-template

---
+++ calc/js/load.js
@@ -0,0 +1,4 @@
+$(window).on('load', function(event) {
+    $('body').removeClass('preload');
+    $('.load').delay(3000).fadeOut('fast');
+});
---
+++ weatherapp/js/load.js
@@ -0,0 +1,4 @@
+$(window).on('load', function(event) {
+    $('body').removeClass('preload');
+    $('.load').delay(1000).fadeOut('fast');
+});
